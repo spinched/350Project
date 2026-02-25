1450ccfef6a45fb9c009b6e4a54b8bfd3b6f9ea1
hlarkin2 <95375078+hlarkin2@users.noreply.github.com>
2026-02-25 12:27:19 -0700

    Add delete statements for multiple records

--- deleteScript.sql
+++ deleteScript.sql
@@ -1,1 +1,14 @@
+DELETE FROM PRODUCT
+WHERE P_Name = 'Pumpkin Juice';
 
+DELETE FROM STOCKER
+WHERE S_FirstName = 'Argus' AND S_LastName = 'Filch';
+
+DELETE FROM LOCATION
+WHERE P_Type = 'Juice';
+
+DELETE FROM MANAGER
+WHERE M_FirstName = 'Minerva' AND M_LastName = 'McGonagall';
+
+DELETE FROM IT
+WHERE IT_FirstName = 'Severus' AND IT_LastName = 'Snape';
